14d4637f96861ffbd399af5470db4249760ee2a4
yostane <yostane@yahoo.fr>
2019-01-24 15:55:45 +0100

    Updated plan

--- news_kotlin_1_3/src/main/kotlin/00_plan.kt
+++ news_kotlin_1_3/src/main/kotlin/00_plan.kt
@@ -1,10 +1,16 @@
-
-
-fun main(){
+fun main() {
     println("Soirée Kotlin avec LAUG \uD83D\uDE0D (22/10/2019)")
     println("Big up aux Sponsors:  \uD83D\uDC4F Worldline et Efficom Lille  \uD83D\uDC4F")
     println("Nouveautés de Kotlin 1.3")
-    val steps = listOf("Programme: \uD83D\uDCDD", "Contracts", "Inline classes", "Script et scrach", "Nouveautés de la Stdlib",
-        "Serialization", "@JvmField et @JvmStatic", "Unsigned int", "Coroutines")
+    val steps = listOf(
+        "Programme: \uD83D\uDCDD", "Contracts",
+        "Inline classes", "Script et scrach",
+        "Nouveautés Stdlib",
+        "Nouveautés diverses",
+        "Serialization",
+        "Nouvelles annotations @JVM",
+        "Unsigned int",
+        "Stabilisation des coroutines"
+    )
     print(steps.joinToString("\n ✅ "))
 }
